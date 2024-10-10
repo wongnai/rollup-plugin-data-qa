b3f2d2439e4df124b70149205cbb37af07db0220
peetck <peetck602@gmail.com>
2024-10-10 10:41:11 +0700

    fix: improve isReactNode (also check _Fragment)

--- src/pluginConstants.ts
+++ src/pluginConstants.ts
@@ -3,3 +3,7 @@ export const IS_E2E_ENABLED = "process.env.E2E_ENABLED === 'true'"
 export const UNCHANGED = null
 
 export const DATA_QA = 'data-qa'
+
+export const JSX_CALLEE_NAMES = ['jsxDEV', 'jsx', '_jsx', 'jsxs', '_jsxs']
+
+export const FRAGMENT_NAMES = ['Fragment', '_Fragment']
